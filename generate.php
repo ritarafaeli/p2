d3301ee1278e14a900f4e82e072bdd782efad2dc
Rita Rafaeli <ritarafaeli@users.noreply.github.com>
2016-01-21 11:14:58 -0500

    Update generate.php

--- generate.php
+++ generate.php
@@ -1,1 +1,32 @@
-<h3>GENERATE</h3>
+<?php
+
+$data = array();
+$data['answer'] = 'test-test-test-test';
+
+$_POST = json_decode(file_get_contents('php://input'), true);
+//$data = file_get_contents("php://input");
+//$request = json_decode($data, TRUE);
+//print_r($request);
+
+
+//TODO:calculate password
+
+
+//response back
+echo json_encode($data);
+
+
+
+
+
+
+
+
+
+
+
+
+// return all our data to an AJAX call
+//echo json_encode($request);
+
+?>
