f948ed61b297e52a89ab24938da97ac2bc32e39c
Rita Rafaeli <ritarafaeli@users.noreply.github.com>
2016-01-21 14:55:40 -0500

    Update generate.php

--- generate.php
+++ generate.php
@@ -4,16 +4,10 @@ $data = array();
 
 
 $_POST = json_decode(file_get_contents('php://input'), true);
-//$data = file_get_contents("php://input");
-//$request = json_decode($data, TRUE);
-//print_r($request);
 
-foreach ($_POST as $param_name => $param_val) {
-    echo " $param_name = $param_val<br />\n";
-}
 
 //dictionary
-$str = "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains";
+$str = "On the other hand we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment so blinded by desire that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will which is the same as saying through shrinking from toil and pain These cases are perfectly simple and easy to distinguish In a free hour when our power of choice is untrammelled and when nothing prevents our being able to do what we like best every pleasure is to be welcomed and every pain avoided But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures or else he endures pains to avoid worse pains";
 $dictionary = explode(" ",$str);
 $size = sizeof($dictionary);
 
@@ -25,14 +19,14 @@ $CASE_UPPER = 'upper';
 $CASE_LOWER = 'lower';
 $CASE_DEFAULT = 'default';
 
-//form inputs
+//form input defaults
 $num = 3;
 $breakage = $BREAKAGE_HYPHENS;
 $case = $CASE_DEFAULT;
 $number = false;
 $specialsymbol = false;
 
-
+//set inputs
 if(!empty($_POST["num"]))
     $num = $_POST["num"];
 
@@ -49,14 +43,19 @@ if(!empty($_POST["specialsymbol"]) && $_POST["specialsymbol"]==1)
    $specialsymbol = true;
 
 
-//TODO:calculate password
-
+//generate password
 $answer = '';
 
 $break = $breakage == $BREAKAGE_SPACES ? " " : "-";
 for($i=0 ; $i < $num ; $i++){
     $rand = rand(1, $size);
-    $word = $case == $CASE_DEFAULT ? $dictionary[$rand-1] : $case == $CASE_UPPER ? strtoupper($dictionary[$rand-1]) : strtolower($dictionary[$rand-1]);
+    $word = ucfirst(strtolower($dictionary[$rand-1]));
+    if($case == $CASE_UPPER){
+        $word = strtoupper($word);
+    }else if($case == $CASE_LOWER){
+        $word = strtolower($word);
+    }
+   
     if($breakage == $BREAKAGE_CAMEL){
         $word = strtolower($word);
         if($i > 0)
@@ -72,17 +71,8 @@ for($i=0 ; $i < $num ; $i++){
 }
 if($specialsymbol)
     $answer .= '@';
+
+//return answer back to AngularJS
 echo $answer;
-
-
-$data['answer'] = $answer;
-
-
-
-//response back
-echo json_encode($data);
-
-// return all our data to an AJAX call
-//echo json_encode($request);
 
 ?>
